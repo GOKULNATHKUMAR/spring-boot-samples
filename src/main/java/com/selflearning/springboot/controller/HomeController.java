5d542cba52252ff7fb21e31c012c2f3f25aa6e58
sanjaikanth <sanjaikanthb.techymeet@gmail.com>
2024-02-02 15:28:11 +0530

    basic configuration codes commit

--- src/main/java/com/selflearning/springboot/controller/HomeController.java
+++ src/main/java/com/selflearning/springboot/controller/HomeController.java
@@ -1,12 +1,18 @@
 package com.selflearning.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-	@GetMapping("/home")
+	
+	@Value("${application.name}")
+	private String appName;
+	
+	@GetMapping("home")
 	public String homeCalling() {
-		return "Home Controller Calling";
+		
+		return "Home Controller Calling :"+appName;
 	}
 }
